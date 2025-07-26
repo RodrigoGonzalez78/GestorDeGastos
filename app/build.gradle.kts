faf84582a09a4601221740f9ad6e37ee1fab7691
RodrigoGonzalez78 <gonzlrodrigo@gmail.com>
2025-07-26 18:03:24 -0300

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.rocketstudio99.gestor_de_gastos"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
